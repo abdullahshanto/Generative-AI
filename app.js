516608b9235c0bdb55af1e93869c3cee494704ae
AbdullahShanto <abdurrahman7328@gmail.com>
2026-07-16 01:39:33 +0600

    updated

--- app.js
+++ app.js
@@ -11,18 +11,36 @@ async function main() {
   }
 
   const completion = await groq.chat.completions.create({
+
+    reasoning_format : {type: 'json_object'},//latest one is json schema 
     model: 'llama-3.1-8b-instant',
     messages: [
       {
         //adding behaviour.persona
         role : 'system',
-        content : "you are shanto's Assistant,you are built for only for Shanto "
+        content : `you are shanto's Assistant,you are built for only for Shanto .Your task is to generate candiadte eveluation score of an ineterview for shanto.Output must be followung json structure
+        {
+           "confidence" : number (1-10 scale),
+           "accuracy" : number (1-10 scale),
+            "pass" : boolean(true/false)
+        }
+
+        this formate must :
+          1.include confidence score of the candidate based on their answers.
+          2.include accuracy score of the candidate based on their answers.
+          3.include pass/fail status based on the scores.
+          4. The output must be in JSON format only, no other text or explanation should be included.
+          5. The output must be a valid JSON object with the specified keys and value types.
+        `
 
       }
       ,
       {
         role: 'user',
-        content: 'who are you?'
+        content: `Evaluate the candidate based on their interview performance.
+          validate the candidate's answers and provide a score for confidence and accuracy on a scale of 1 to 10.
+           
+        `
       }
     ]
   });
